687b534a93053b8f4a91848f214890fa42909a74
Jash Patel <113690837+jashitgithub@users.noreply.github.com>
2025-03-17 04:57:54 +0000

    datatypes

--- 01_basics/test.js
+++ 01_basics/test.js
@@ -1,4 +1,24 @@
-const acaccountId = 1234;
+const accountID = 1234;
 let accountName = "jash";
 var accountCity = "Bilimora"
 accounrPerferace = "Saving"
+
+let accountID2 = 5678;
+accountName = "current"
+// accountID = 6543;
+
+console.table([accountID, accountName, accountCity, accounrPerferace]);
+
+// Datatypes and ECMA standards : 
+// NOTE : Code Redability should be high.
+
+// Premitive datatypes
+// number => 2 to power 53
+// bigint => Bigger number
+// string => "", '', 
+// boolean => Han ya naa ./ True or false
+// null => standalone value ("it is also object it's type is object")
+// undefined => value not assign
+// symbol => unique   
+
+// object 
